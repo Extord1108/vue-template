8384c8ad0b301a6df01d6f875b320d6a49b6853b
Re-Li-fe <gyf1657261410@outlook.com>
2022-10-25 10:40:40 +0800

    the second one

---
+++ template2/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes:RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home/index.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/index.vue')
+  }
+]
+
+const router = createRouter({
+  // history: createWebHashHistory(), // hash 路由模式
+  history: createWebHistory(), // history 路由模式
+  routes // 路由规则
+})
+
+export default router
+
