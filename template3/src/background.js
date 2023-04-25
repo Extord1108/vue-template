fa46fadaa2e4c887df27db7f4c7ef22cf1220b64
Re-Li-fe <gyf1657261410@outlook.com>
2023-04-25 16:12:03 +0800

    修改template3

--- template3/src/background.js
+++ template3/src/background.js
@@ -1,6 +1,5 @@
 const { app, protocol, BrowserWindow, Menu } = require("electron");
 const { createProtocol } = require("vue-cli-plugin-electron-builder/lib");
-const WinState = require("electron-win-state").default;
 import initIpcEvent from "./modules/ipcEvent";
 
 // Scheme must be registered before the app is ready
@@ -11,14 +10,16 @@ protocol.registerSchemesAsPrivileged([
 //禁用菜单栏
 Menu.setApplicationMenu(null);
 
-const createWindow = async () => {
-  const winState = new WinState({
-    defaultWidth: 800,
-    defaultHeight: 600,
-  });
+//定义多个窗口
+var win1 = null;
+var win2 = null;
+
+const createWindow = async (devPath, prodPath) => {
   // 创建窗口
   const win = new BrowserWindow({
-    ...winState.winOptions,
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, "../public/logo.png"),
     webPreferences: {
       preload: __dirname + "/preload.js",
     },
@@ -27,36 +28,40 @@ const createWindow = async () => {
   //挂载vue页面
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL+devPath);
+    win.webContents.openDevTools();
   } else {
-    createProtocol("app");
     // Load the index.html when not in development
-    win.loadURL("app://./index.html");
+    win.loadURL(`app://./${prodPath}`);
   }
-
-  // 打开开发者工具,打包时需要关闭
-  win.webContents.openDevTools();
-
-  //使用winState管理窗口，会自动保存窗口的位置和大小
-  winState.manage(win);
 
   //禁止安全警告
   process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
+  return win;
 };
 
 //初始化主进程监听事件
 initIpcEvent();
 
 app.on("ready", async () => {
-  createWindow();
-  app.on("activate", () => {
-    // 在 macOS 系统内, 如果没有已开启的应用窗口
-    // 点击托盘图标时通常会重新创建一个新窗口
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+  if (!process.env.WEBPACK_DEV_SERVER_URL) {
+    createProtocol("app");
+  }
+  createWindow("window1", "index1.html").then((res) => {
+    win1 = res;
   });
+
+  //初始化主进程监听事件
+  initIpcEvent();
+});
+app.on("activate", () => {
+  // 在 macOS 系统内, 如果没有已开启的应用窗口
+  // 点击托盘图标时通常会重新创建一个新窗口
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow("window1", "index1.html").then((res) => {
+      win1 = res;
+    });
+  }
 });
 
 // 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
---
+++ template3/src/window2/views/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <el-button @click="mkdir">创建文件</el-button>
+  </div>
+</template>
+<script setup>
+const mkdir = () => {
+  window.electron.handleMkdir();
+};
+</script>
+<style scoped lang="less"></style>
